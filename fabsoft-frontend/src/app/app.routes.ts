aa9805a7c507c26e210cfe0dfc5f15a630ff1456
Lucas Felipe <123039946+Lusca1337@users.noreply.github.com>
2025-11-14 20:05:49 +0000

    Começo do FrontEnd

--- fabsoft-frontend/src/app/app.routes.ts
+++ fabsoft-frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FilmeComponent } from './filme/filme.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'filme', component: FilmeComponent}
+];
--- fabsoft-frontend/src/app/filme/filme.component.ts
+++ fabsoft-frontend/src/app/filme/filme.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { Filme } from '../model/filme';
+import { FilmeService } from '../service/filme.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-filme',
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './filme.html',
   styleUrl: './filme.css',
+  providers: [FilmeService]
 })
-export class Filme {
+
+export class FilmeComponent {
+
+  listaFilmes: Filme[] = []
+
+  constructor(private filmeService: FilmeService){}
+
+  ngOnInit(){
+    console.log('Carregando Filmes...')
+    this.filmeService.getFilme().subscribe( filmes => {
+      this.listaFilmes = filmes
+    })
+  }
 
 }
