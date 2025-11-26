097eef808d0cdce44802e2549ec92b3734fc8aef
Fellipe Negherbon Prim <fellipe.prim@univille.br>
2025-11-26 05:33:50 +0000

    Projeto Finalizado

--- fabsoft-backend/src/main/java/br/univille/fabsoft_backend/controller/AuthController.java
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/controller/AuthController.java
@@ -1,7 +1,7 @@
 package br.univille.fabsoft_backend.controller;
 
 import br.univille.fabsoft_backend.entity.Usuario;
-import br.univille.fabsoft_backend.entity.TipoUsuario; // Importe seu Enum
+import br.univille.fabsoft_backend.entity.TipoUsuario;
 import br.univille.fabsoft_backend.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/Filme.java
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/Filme.java
@@ -1,6 +1,8 @@
 package br.univille.fabsoft_backend.entity;
 
 import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +16,7 @@ public class Filme {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String titulo;
+    @Column(columnDefinition = "TEXT")
     private String sinopse;
     private int horas;
     private int minutos;   
